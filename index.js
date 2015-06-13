efb6a8f8b961b2aa8fe55ffa1d7ca4a3c21619ef
elad do <Elad@elads-MacBook-Pro.local>
2015-06-13 14:38:58 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Elad on 6/12/15.
+ */
+
+var Publisher = require('./lib/Publisher'),
+    Subscriber = require('./lib/Subscriber');
+
+module.exports = {
+    Publisher: Publisher,
+    Subscriber: Subscriber
+};
